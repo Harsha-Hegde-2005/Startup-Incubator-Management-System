247c97bbfe5a337299ff5f6fe86aa8179a18d980
Harsha Hegde <harshahegde12345@gmail.com>
2025-11-06 14:34:12 +0530

    Add files via upload

---
+++ GUI/db_config.py
@@ -0,0 +1,10 @@
+# db_config.py
+
+# !! IMPORTANT !!
+# Update these values with your own MySQL username and password.
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': USER_NAME,  # e.g., 'root'
+    'password': USER_PASSWORD, # e.g., 'root'
+    'database': 'startup_incubator_management_system'
+}
